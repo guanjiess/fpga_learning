37866606f5d220c715951bef8f6749e0b25c9311
gsx_laptop <2280933632@qq.com>
2025-01-16 23:31:31 +0800

    create ip_top for practicing

---
+++ ip_top.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ns
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// Create Date: 2025/01/16 22:54:19
+// Design Name: 
+
+module ip_top(
+    input i_clk,
+    output locked
+    );
+    
+	wire 		w_clk_100m	;
+	wire 		w_clk_50m	;
+	wire 		w_clk_25m	;
+	wire		w_locked	;
+	
+	reg [10:0]	r_count1;
+	reg [10:0]	r_count2;
+	reg [10:0]	r_count3;
+	
+	
+clk_generate clk_generate1 
+ (
+  .clk_out1	(	w_clk_100m		),
+  .clk_out2	(	w_clk_50m		),
+  .clk_out3	(	w_clk_25m		),
+  .reset	(	0				),
+  .locked	(	w_locked		),
+  .clk_in1	(	i_clk			)
+ );
+ 
+ always @(posedge w_clk_100m) begin
+	if(rst) begin
+		r_count1 <= 0;
+	end else begin
+		r_count1 <= r_count1 + 1;
+	end else begin
+		r_count1 <= r_count1;
+	end
+ end
+ 
+ always @(posedge w_clk_50m) begin
+	if(rst) begin
+		r_count2 <= 0;
+	end else begin
+		r_count2 <= r_count2 + 1;
+	end else begin
+		r_count2 <= r_count2;
+	end
+ end
+ 
+ 
+ always @(posedge w_clk_25m) begin
+	if(rst) begin
+		r_count3 <= 0;
+	end else begin
+		r_count3 <= r_count3 + 1;
+	end else begin
+		r_count3 <= r_count2;
+	end
+ end
+ 
+    
+endmodule
