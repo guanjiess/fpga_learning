a432ab85fea82422a4c7c85ec8bed58ea6ac8600
guanshixun <guanjie479@gmail.com>
2025-01-17 16:50:04 +0800

    ram tested

--- ip_top.v
+++ ip_top.v
@@ -17,31 +17,8 @@ module ip_top(
 	wire		w_locked	;
 	
 	reg [10:0]	r_count1 = 0;
-	reg [10:0]	r_count2 = 0;
+	reg [15:0]	r_count2 = 0;
 	reg [10:0]	r_count3 = 0;
-	
-	
-clk_generate clk_generate1 
- (
-  .clk_out1	(	w_clk_100m		),
-  .clk_out2	(	w_clk_50m		),
-  .clk_out3	(	w_clk_25m		),
-  .reset	(	rst				),
-  .locked	(	w_locked		),
-  .clk_in1	(	i_clk			)
- );
- 
- BRAM_16X2K ram1(
-  .clka		(	w_clk_50m		),
-  .ena		(		1			),
-  .wea		(					),
-  .addra	(					),
-  .dina		(					),
-  .clkb		(	w_clk_50m		),
-  .enb		(					),
-  .addrb	(					),
-  .doutb    (					)
-);
  
  always @(posedge w_clk_100m) begin
 	if(rst) begin
@@ -67,6 +44,64 @@ clk_generate clk_generate1
 		r_count3 <= r_count3 + 1;
 	end
  end
+ // write and read data
+ reg 	[15:0]		r_data_in = 0;
+ reg				r_wea = 0;
+ reg	[11:0]		r_addra = 0;
+ always @(posedge w_clk_50m) begin
+	if(rst) begin
+		r_data_in <= 0;
+		r_wea <= 0;
+		r_addra <= 0;
+	end else if(r_count2 >= 20 && r_count2 <= 20 + 2047) begin
+		r_addra <= r_addra + 1;
+		r_data_in <= r_count2;
+		r_wea <= 1;
+	end else begin
+		r_wea <= 0;
+		r_data_in <= 0;
+		r_addra <= 0;
+	end
+ end
+ 
+ wire 	[15:0]		w_data_out;
+ reg				r_enb = 0;
+ reg	[11:0]		r_addrb = 0;
+ always @(posedge w_clk_50m) begin
+	if(rst) begin
+		r_enb <= 0;
+		r_addrb <= 0;
+	end else if(r_count2 >= 20 + 2047 + 100 && r_count2 <= 20 + 2047 + 100 + 2047 + 1) begin
+		r_addrb <= r_addrb + 1;
+		r_enb <= 1;
+	end else begin
+		r_addrb <= 0;
+		r_enb <= 0;
+	end
+ end
+ 
+ clk_generate clk_generate1 
+ (
+  .clk_out1	(	w_clk_100m		),
+  .clk_out2	(	w_clk_50m		),
+  .clk_out3	(	w_clk_25m		),
+  .reset	(	rst				),
+  .locked	(	w_locked		),
+  .clk_in1	(	i_clk			)
+ );
+ 
+ BRAM_16X2K ram1(
+  .clka		(	w_clk_50m		),
+  .ena		(		1			),
+  .wea		(		r_wea		),
+  .addra	(		r_addra		),
+  .dina		(		r_data_in	),
+  .clkb		(		w_clk_50m	),
+  .enb		(		r_enb		),
+  .addrb	(		r_addrb		),
+  .doutb    (		w_data_out	)
+);
+ 
  
     
 endmodule
